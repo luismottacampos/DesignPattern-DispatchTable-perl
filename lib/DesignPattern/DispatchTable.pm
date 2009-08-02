5b7ab7331e39fa64b77b84d61d875908beb9c6e3
Luis Motta Campos <lcampos@LMCs-MacBook.local>
2009-08-02 12:08:06 +0200

    Low battery, mid-way through the implementation. Probably broken code at the moment.

--- lib/DesignPattern/DispatchTable.pm
+++ lib/DesignPattern/DispatchTable.pm
@@ -5,7 +5,7 @@ use strict;
 
 =head1 NAME
 
-DesignPattern::DispatchTable - The great new DesignPattern::DispatchTable!
+DesignPattern::DispatchTable - Implementation of a reusable versatile dispatch table.
 
 =head1 VERSION
 
@@ -15,17 +15,26 @@ Version 0.01
 
 our $VERSION = '0.01';
 
-
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
+This module implements the DispatchTable design pattern, for code reuse,
+readability, organisation, better coding style, and fun.
 
     use DesignPattern::DispatchTable;
 
-    my $foo = DesignPattern::DispatchTable->new();
-    ...
+    my $dispatch_table = DesignPattern::DispatchTable;
+    $dispatch_table->register( $string     => $code_ref );
+    $dispatch_table->register( qr{regexp?} => $code_ref );
+
+    # Or even
+
+    my $dispatch_table = DesignPattern::DispatchTable(
+        $string     => $code_ref,
+        qr{regexp?} => $code_ref
+    );
+
+    # and then you might call:
+    $dispatch_table->call($argument);    # calls the first matching method
 
 =head1 EXPORT
 
@@ -34,18 +43,109 @@ if you don't export anything, such as for a purely object-oriented module.
 
 =head1 FUNCTIONS
 
-=head2 function1
+=head2 new
+
+Constructor. Returns a new Dispatch Table. Accepts an optional list of C<<
+$condition => $code_ref >> pairs to be installed in the Dispatch Table.
 
 =cut
 
-sub function1 {
+sub new {
+    my $class = shift;
+    my $self = bless [], $class;
+    $self->register(@_) if @_;
+    return $self;
 }
 
-=head2 function2
+=head2 register
+
+Register accepts one or more pairs of C<< $condition => $code_ref >> and
+install them in the Dispatch Table. Returns a true value if all values were
+installed successfuly (i.e., there is no invalid values as key and all keys
+have a proper code reference to go with it).
+
+New versions of handlers (and corresponding decision code) are installed in the
+beginining of the list. This makes for an implicit guarantee of reverse order:
+I will always check first for the latest installed (decision, handlers) code
+pairs.
 
 =cut
 
-sub function2 {
+# XXX: is there any sane way to check if a sub is already installed?
+# XXX: is there any sane way to check if a Regexp is already installed?
+# XXX: should I worry about duplicates?
+
+sub register {
+    my $self = shift;
+    my @new_handlers;
+    while ( my ( $decision, $handler ) = splice @_, 0, 2 ) {
+        return 0
+          unless defined( ref $decision )
+              || (   UNIVERSAL::isa( $decision, 'Regexp' )
+                  || UNIVERSAL::isa( $decision, 'CODE' ) );
+        push @new_handlers,
+          {
+            type => ref $decision || 'STRING',
+            decision => $decision,
+            handler  => $handler
+          };
+    }
+
+    # Install new handlers in the begining of the list, prevents call() from
+    # reaching the first version of duplicated methods.
+    return unshift @$self, @new_handlers;
+}
+
+=head2 call
+
+Call receives one or more values on which to make a decision about, and calls
+the first matching decision on the values. If the decision was made with a
+regular expression, it also arranges so the captured values are passed to the
+code reference registered.
+
+TODO: in case we allow passing in subroutines as decision making processes, we
+should consider passing in the return value from the decision sub to the call
+handler.
+
+=cut
+
+sub call {
+    my ( $self,    $argument )  = @_;
+    my ( $handler, @arguments ) = $self->code_for($argument);
+    my @return_value = eval { $handler->(@arguments) };
+    die qq{Error calling handler for $argument: $@} if $@;
+    return @return_value;
+}
+
+=head2 code_for
+
+Obtains the code for the given argument, and returns it. Used internally to
+fetch the code handler bound to a specific (code|string|regex) in the dispatch
+table.
+
+=cut
+
+sub code_for {
+    my ( $self, $argument ) = @_;
+    foreach my $entry (@$self) {
+        my @results;
+
+       # TODO: Find a way to implement this as a DesignPattern::DispatchTable :)
+        if ( $entry->{type} eq 'STRING' && $entry->{decision} eq $argument ) {
+            return $entry->{handler}, ();
+        }
+        elsif ( $entry->{type} eq 'Regexp'
+            && ( @results = $argument =~ m{$$entry{decision}} ) )
+        {
+            return $entry->{handler}, @results;
+        }
+        elsif ($entry->{type} eq 'CODE'
+            && ( @results = $entry->{handler}->($argument) )
+            && @results )
+        {
+            return $entry->{handler}, @results;
+        }
+    }
 }
 
 =head1 AUTHOR
@@ -104,4 +204,4 @@ under the same terms as Perl itself.
 
 =cut
 
-1; # End of DesignPattern::DispatchTable
+1;    # End of DesignPattern::DispatchTable
--- t/01-basics.t
+++ t/01-basics.t
@@ -2,7 +2,10 @@ use Test::More qw( no_plan );
 
 BEGIN { our $class = 'DesignPattern::DispatchTable'; use_ok $class; }
 
-isa_ok $dt = $class->new(), q{I can create an empty dispatch table...};
+can_ok $class, qw( register call );
+
+isa_ok $dt = $class->new(), $class, q{I can create an empty dispatch table...};
+
 ok $dt->register( 'METHOD CALL' => sub { return 1 } ),
   q{   ... and add a string based call};
 ok $dt->register( qr{(THIS|THAT)} => sub { return 2 } ),
@@ -13,13 +16,7 @@ ok $dt->call('METHOD CALL') == 1,
 ok $dt->call('THIS') == 2,      q{   ... and my regular expression triggers};
 ok $dt->call('EVEN THAT') == 2, q{   ... for all alternatives};
 
-ok ref( $code = $dt->code_for('METHOD CALL') ) eq 'CODE',
-  q{I can fetch the sub corresponding to 'METHOD CALL'};
-ok $code->() == 1, q{   ... and calling it returns the right value};
-
-ok !$dt->register( 'METHOD CALL' => sub { return 3 } ),
-  q{I can't register the same key twice};
-ok $dt->register( 'METHOD CALL' => sub { return 3 }, 'override' ),
-  q{   ... unless I pass in a "force" option };
-ok $dt->call('METHOD CALL') == 3, q{   ... and the old value is discarded};
+ok $dt->register( 'METHOD CALL' => sub { return 3 } ),
+  q{Registering the same key twice discards the previous handler};
+ok $dt->call('METHOD CALL') == 3, q{   ... and the new code is used};
 
